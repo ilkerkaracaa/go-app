45fedb8122f73c0cc362fd128bd83e0a1a730394
İlker <illkerrkaraca@gmail.com>
2024-06-10 13:19:04 +0300

    added product_repository_test func

--- test/infrastructure/product_repository_test.go
+++ test/infrastructure/product_repository_test.go
@@ -5,10 +5,12 @@ import (
 	"fmt"
 	"goapp/common/postgresql"
 	"goapp/datalayer"
+	"goapp/domain"
 	"os"
 	"testing"
 
 	"github.com/jackc/pgx/v4/pgxpool"
+	"github.com/stretchr/testify/assert"
 )
 
 var productRepositorty datalayer.IProductRepository
@@ -41,6 +43,40 @@ func clear(ctx context.Context, dbPool *pgxpool.Pool) {
 }
 func TestGetAllProduct(t *testing.T) {
 	setup(ctx, dbPool)
-	fmt.Println("Test all products")
+	expected := []domain.Product{
+		{
+			Id:       1,
+			Name:     "AirFryer",
+			Price:    3000.0,
+			Discount: 22.0,
+			Store:    "ABC TECH",
+		},
+		{
+			Id:       2,
+			Name:     "Ütü",
+			Price:    1500.0,
+			Discount: 10.0,
+			Store:    "ABC TECH",
+		},
+		{
+			Id:       3,
+			Name:     "Çamaşır Makinesi",
+			Price:    10000.0,
+			Discount: 15.0,
+			Store:    "ABC TECH",
+		},
+		{
+			Id:       4,
+			Name:     "Lambader",
+			Price:    2000.0,
+			Discount: 0.0,
+			Store:    "Dekorasyon Sarayı",
+		},
+	}
+	t.Run("Get all products", func(t *testing.T) {
+		actualProducts := productRepositorty.GetAllProducts()
+		assert.Equal(t, 4, len(actualProducts))
+		assert.Equal(t, expected, actualProducts)
+	})
 	clear(ctx, dbPool)
 }
