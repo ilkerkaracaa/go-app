e5b67df8b7dbdf6753f84ecb2f194236ea0e5f23
İlker <illkerrkaraca@gmail.com>
2024-06-12 18:54:46 +0300

    created get post and update controller

--- controller/product_controller.go
+++ controller/product_controller.go
@@ -1,7 +1,11 @@
 package controller
 
 import (
+	"goapp/controller/request"
+	"goapp/controller/response"
 	"goapp/service"
+	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 )
@@ -24,6 +28,76 @@ func (productController *ProductController) RegisterRoutes(e *echo.Echo) {
 	e.DELETE("/api/v1/products/:id", productController.DeleteProductById)
 }
 
-func (productController *ProductController) GetProductById(id string) (*echo.Echo, error) {
-	return nil, nil
+func (productController *ProductController) GetProductById(c echo.Context) error {
+	param := c.Param("id")
+	productId, _ := strconv.Atoi(param)
+
+	product, err := productController.productService.GetById(int64(productId))
+	if err != nil {
+		return c.JSON(http.StatusNotFound, response.ErrorResponse{
+			ErrorDescription: err.Error(),
+		})
+	}
+	return c.JSON(http.StatusOK, response.ToResponse(product))
+}
+
+func (productController *ProductController) GetAllProducts(c echo.Context) error {
+	store := c.QueryParam("store")
+	if len(store) == 0 {
+		allProducts := productController.productService.GetAllProducts()
+		return c.JSON(http.StatusOK, response.ToResponseList(allProducts))
+	}
+	productsWithGivenStore := productController.productService.GetAllProductsByStore(store)
+	return c.JSON(http.StatusOK, response.ToResponseList(productsWithGivenStore))
+}
+
+func (productController *ProductController) AddProduct(c echo.Context) error {
+	var addProductRequest request.AddProductRequest
+	bindErr := c.Bind(&addProductRequest)
+	if bindErr != nil {
+		return c.JSON(http.StatusBadRequest, response.ErrorResponse{
+			ErrorDescription: bindErr.Error(),
+		})
+	}
+	err := productController.productService.Add(addProductRequest.ToModel())
+
+	if err != nil {
+		return c.JSON(http.StatusUnprocessableEntity, response.ErrorResponse{
+			ErrorDescription: err.Error(),
+		})
+	}
+	return c.NoContent(http.StatusCreated)
+}
+
+func (productController *ProductController) UpdatePrice(c echo.Context) error {
+	param := c.Param("id")
+	productId, _ := strconv.Atoi(param)
+
+	newPrice := c.QueryParam("newPrice")
+	if len(newPrice) == 0 {
+		return c.JSON(http.StatusBadRequest, response.ErrorResponse{
+			ErrorDescription: "Parameter newPrice is required!",
+		})
+	}
+	convertedPrice, err := strconv.ParseFloat(newPrice, 32)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, response.ErrorResponse{
+			ErrorDescription: "NewPrice Format Disrupted!",
+		})
+	}
+	productController.productService.UpdatePrice(int64(productId), float32(convertedPrice))
+	return c.NoContent(http.StatusOK)
+}
+
+func (productController *ProductController) DeleteProductById(c echo.Context) error {
+	param := c.Param("id")
+	productId, _ := strconv.Atoi(param)
+
+	err := productController.productService.DeleteById(int64(productId))
+	if err != nil {
+		return c.JSON(http.StatusNotFound, response.ErrorResponse{
+			ErrorDescription: err.Error(),
+		})
+	}
+	return c.NoContent(http.StatusOK)
 }
